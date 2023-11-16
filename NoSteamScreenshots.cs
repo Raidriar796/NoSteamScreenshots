51a696816d14f4a0971dd8e64a24333135976a5b
Raidriar796 <145519075+Raidriar796@users.noreply.github.com>
2023-11-16 01:05:28 -0800

    First release

---
+++ NoSteamScreenshots.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using ResoniteModLoader;
+using System;
+using System.Reflection;
+using FrooxEngine;
+
+namespace NoSteamScreenshots;
+
+public class NoSteamScreenshots : ResoniteMod
+{
+    public override string Name => "NoSteamScreenshots";
+    public override string Author => "Raidriar796";
+    public override string Version => "1.0.0";
+    public override string Link => "https://github.com/Raidriar796/NoSteamScreenshots";
+    public static ModConfiguration? Config;
+
+    [AutoRegisterConfigKey] public static readonly ModConfigurationKey<bool> Enabled =
+            new ModConfigurationKey<bool>(
+                "Enabled",
+                "Allow photos to save to Steam.",
+                () => false);
+
+    public override void OnEngineInit()
+    {
+        Harmony harmony = new("net.Raidriar796.NoSteamScreenshots");
+        Config = GetConfiguration();
+        Config!.Save(true);
+        harmony.PatchAll();
+    }
+
+    [HarmonyPatch(typeof(SteamConnector), "NotifyOfScreenshot")]
+    class ScreenshotPatch
+    {
+        static bool Prefix() 
+        {
+            if (Config.GetValue(Enabled)) return true;
+
+            return false;
+        }
+    }
+}
